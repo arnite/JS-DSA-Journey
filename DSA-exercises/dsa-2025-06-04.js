a07c3cb7b0f725fca90cfe6441a2ac3b950f01bc
arnite <udembao429@gmail.com>
2025-06-04 12:48:43 +0100

    another update

---
+++ DSA-exercises/dsa-2025-06-04.js
@@ -0,0 +1,45 @@
+// i know ia haven't been consisitent for a while now, but i hope to resolve that.
+
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+*/
+
+function plusOne(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] < 9) {
+      arr[i] += 1;
+      return arr;
+    }
+
+    arr[i] = 0;
+  }
+
+  arr.unshift(1);
+  return arr;
+}
+
+const arr = [6, 2];
+const output = plusOne(arr);
+console.log(output);
+
+// NOTE: i had help from A.I concerning some parts, but i can also see my prgress because i have gotten better at javascript syntax..so i can say it is going well.
