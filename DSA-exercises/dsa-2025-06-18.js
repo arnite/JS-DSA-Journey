29cede5319da20e0e856dc2c882cb224fc465809
arnite <udembao429@gmail.com>
2025-06-18 12:53:39 +0100

    update

--- DSA-exercises/dsa-2025-06-18.js
+++ DSA-exercises/dsa-2025-06-18.js
@@ -1,1 +1,26 @@
-//Today we solving DSA focusing on string operations 
+//Today we solving DSA focusing on string operations
+// Came accross a roman numerals question, broo, this question is very hard...but we will work it out, found out roman numerals end at 3999.. we learn everyday.
+
+function romanToInt(s) {
+    const romanMap = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    s = s.toUpperCase();
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+
+        if (next > current) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+}
+
+const s = "iv";
+const output = romanToInt(s);
+console.log(output);
